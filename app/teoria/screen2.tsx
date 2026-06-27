7b8ea2c785723f68d0239d5119fe32131d316a3c
juampimedina06 <jpmedinagomez1@gmail.com>
2026-06-27 07:54:49 -0300

    subiendo ejercio1

---
+++ app/teoria/screen2.tsx
@@ -0,0 +1,21 @@
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { Button, View } from "react-native";
+import styled from "styled-components/native";
+
+export default function Screen2() {
+  const router = useRouter();
+  const data = useLocalSearchParams();
+  return (
+    <View>
+      <Title>
+        Hola {data.nombre} espero que disfrutes el curso de {data.curso}
+      </Title>
+      <Button title="Volver" onPress={() => router.back()} />
+    </View>
+  );
+}
+
+const Title = styled.Text`
+  font-size: 50px;
+  color: blue;
+`;
---
+++ styles/theme.tsx
@@ -0,0 +1,35 @@
+export const darkTheme = {
+  colors: {
+    primary: "#bb86fc",
+    secondary: "#03dac6",
+    background: "#121212",
+    text: "#ffffff",
+    button: {
+      background: "#bb86fc",
+      text: "#000000",
+    },
+  },
+  fonts: {
+    large: "20px",
+    medium: "16px",
+    small: "12px",
+  },
+};
+
+export const lightTheme = {
+  colors: {
+    primary: "#6200ee",
+    secondary: "#03dac6",
+    background: "#ffffff",
+    text: "#000000",
+    button: {
+      background: "#6200ee",
+      text: "#ffffff",
+    },
+  },
+  fonts: {
+    large: "20px",
+    medium: "16px",
+    small: "12px",
+  },
+};
